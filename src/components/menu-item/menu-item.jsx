5ba7065b381ec0775c770341855098a37ae97d2a
tiafreeman <tiajane.freeman@gmail.com>
2020-05-22 15:15:07 +1000

    refactoring to create the menu and directory components

--- src/components/menu-item/menu-item.jsx
+++ src/components/menu-item/menu-item.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-import { render } from "node-sass";
 
-const MenuItem = ({ title }) => {
-  render(
-    <div className="menu-item">
-      <div className="content">
-        <h1 className="title">{title}</h1>
-        <span className="subtitle">SHOP NOW</span>
-      </div>
+import "./menu-item.scss";
+
+// eslint-disable-next-line react/prop-types
+const MenuItem = ({ title }) => (
+  <div className="menu-item">
+    <div className="content">
+      <h1 className="title">{title}</h1>
+      <span className="subtitle">SHOP NOW</span>
     </div>
-  );
-};
+  </div>
+);
 
 export default MenuItem;

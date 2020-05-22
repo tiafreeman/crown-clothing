295e951898851942cf4b9f479d662c93406cc903
tiafreeman <tiajane.freeman@gmail.com>
2020-05-22 16:17:30 +1000

    adding menu item styling and animations

--- src/components/menu-item/menu-item.jsx
+++ src/components/menu-item/menu-item.jsx
@@ -1,12 +1,19 @@
+/* eslint-disable react/prop-types */
 import React from "react";
 
 import "./menu-item.scss";
 
 // eslint-disable-next-line react/prop-types
-const MenuItem = ({ title }) => (
-  <div className="menu-item">
+const MenuItem = ({ title, imageUrl, size }) => (
+  <div className={`${size} menu-item`}>
+    <div
+      className="background-img"
+      style={{
+        backgroundImage: `url(${imageUrl})`,
+      }}
+    />
     <div className="content">
-      <h1 className="title">{title}</h1>
+      <h1 className="title">{title.toUpperCase()}</h1>
       <span className="subtitle">SHOP NOW</span>
     </div>
   </div>

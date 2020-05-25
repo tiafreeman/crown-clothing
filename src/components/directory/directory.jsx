10dd326cef279762ea4bf00fbc950e8b5be7ac84
tiafreeman <tiajane.freeman@gmail.com>
2020-05-25 18:38:30 +1000

    fixing linting errors

--- src/components/directory/directory.jsx
+++ src/components/directory/directory.jsx
@@ -51,12 +51,7 @@ class Directory extends React.Component {
     return (
       <div className="directory-menu">
         {this.state.sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem
-            key={id}
-            title={title}
-            imageUrl={imageUrl}
-            size={size}
-          />
+          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
         ))}
       </div>
     );

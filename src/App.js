37ab7ee26dc29d8049f6148e4cb02a6a696ae2c4
tiafreeman <tiajane.freeman@gmail.com>
2020-05-25 20:48:50 +1000

    added header component

--- src/App.js
+++ src/App.js
@@ -5,16 +5,18 @@ import "./App.css";
 
 import HomePage from "./pages/homepage/homepage";
 import ShopPage from "./pages/shop/shop";
+import Header from "./components/header/header";
 
-class App extends React.PureComponent {
-  render() {
-    return (
+function App() {
+  return (
+    <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
       </Switch>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;

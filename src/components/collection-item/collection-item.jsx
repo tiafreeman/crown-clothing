2798ebb167e44c40aed4cabecc2655cbe7ee5e0e
tiafreeman <tiajane.freeman@gmail.com>
2020-05-25 20:20:11 +1000

    added shop page with item previews

---
+++ src/components/collection-item/collection-item.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import "./collection-item.scss";
+
+const CollectionItem = (items) => (
+  <div className="collection-item">
+    <div
+      className="image"
+      style={{
+        backgroundImage: `url(${items.imageUrl})`,
+      }}
+    />
+    <div className="collection-footer">
+      <span className="name">{items.name}</span>
+      <span className="price">{items.price}</span>
+    </div>
+  </div>
+);
+
+export default CollectionItem;
--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,15 +1,27 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React, { Component } from "react";
 import shopData from "./shopData";
+
+import CollectionPreview from "../../components/collection-preview/collection-preview";
 
 class ShopPage extends Component {
   constructor(props) {
     super(props);
 
-    this.props = shopData;
+    this.state = {
+      collections: shopData,
+    };
   }
 
   render() {
-    return <div>Shop Page</div>;
+    const { collections } = this.state;
+    return (
+      <div className="shop-page">
+        {collections.map(({ id, ...otherCollectionProps }) => (
+          <CollectionPreview key={id} {...otherCollectionProps} />
+        ))}
+      </div>
+    );
   }
 }
 

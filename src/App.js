e62b69eae8acf53ab3825fc646b375cf07e898e0
tiafreeman <tiajane.freeman@gmail.com>
2020-05-29 19:02:50 +1000

    connected to the firebase database so users are now being stored

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { auth } from './firebase/firebase.utils';
+import {
+  auth,
+  createUserProfileDocument,
+} from './firebase/firebase.utils';
 
 import './App.css';
 
@@ -21,9 +24,26 @@ class App extends Component {
   }
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
-    });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(
+      async (userAuth) => {
+        if (userAuth) {
+          const userRef = await createUserProfileDocument(userAuth);
+
+          userRef.onSnapshot((snapShot) => {
+            this.setState({
+              currentUser: {
+                id: snapShot.id,
+                ...snapShot.data(),
+              },
+            });
+
+            console.log(this.state);
+          });
+        }
+
+        this.setState({ currentUser: userAuth });
+      },
+    );
   }
 
   componentWillUnmount() {

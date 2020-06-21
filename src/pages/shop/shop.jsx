652c9a8dca95dc7cbe6d6036667726f58b8eda38
tiafreeman <tiajane.freeman@gmail.com>
2020-06-21 20:40:54 +1000

    adding the pages for each of the different collections

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,11 +1,22 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
+import { Route } from 'react-router-dom';
 
+import CollectionPage from '../collection/collection';
 import CollectionsOverview from '../../components/collections-overview/collections-overview';
 
-const ShopPage = () => {
+const ShopPage = ({ match }) => {
   return (
     <div className="shop-page">
-      <CollectionsOverview />
+      <Route
+        exact
+        path={`${match.path}`}
+        component={CollectionsOverview}
+      />
+      <Route
+        path={`${match.path}/:collectionId`}
+        component={CollectionPage}
+      />
     </div>
   );
 };
